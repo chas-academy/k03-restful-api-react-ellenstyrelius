7fae49f3be2ab132298c9e7c2f1a92f201ca59dd
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-09-10 17:50:27 +0200

    Change eslinConfig in client package.json to also extend prettier and not only react-app and change errors coming from prettier to warnings. Add some more prettier config to root package.json. Run prettier on App.js and index.html in client.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": ["plugin:prettier/recommended"],
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-unused-vars": "warn"
-    }
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ['plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-unused-vars': 'warn'
+  }
 };
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 function App() {
   return (
     <div>
-      <p>Coming soon: Tingling Tints webshop :)</p>
+      <p>Coming soon: Tingling Tints webshop</p>
     </div>
   );
 }
