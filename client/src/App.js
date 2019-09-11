9d7ca9cbc09d8e2197e72d7f252b27a90ea1ad95
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-09-11 08:25:31 +0200

    Make one silly change to App.jsin client to check that netlify continuous deployment is working as it should.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <p>Coming soon: Tingling Tints webshop</p>
+      <p>Coming soon: Tingling Tints webshop (°-°)</p>
     </div>
   );
 }
