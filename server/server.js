29bdb427aebf596c7ced7e91c0ce86576e42f150
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-09-15 14:32:22 +0200

    Add config folder with db file in it, containing exported object with uri to my MongoDB. Add config to .gitignore in server. Add connection to Mongo with mongoose in server.js. Add port variable that either contains the env port variable that Heroku uses or port 5000, the latter for local use. Add listen method to tell the express app to listen to whatever port is set in the port variable.

--- server/server.js
+++ server/server.js
@@ -7,4 +7,15 @@ const app = express();
 // body-parser middleware
 app.use(bodyParser.json());
 
-console.log('hello this is server :)');
+// database config
+const db = require('./config/db').mongoURI;
+
+// connect to mongo
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB is connected'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
