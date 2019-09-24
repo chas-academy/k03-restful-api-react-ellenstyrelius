529c849ec4dc5c5141876057ba518f8fd6cbffd6
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-09-24 16:56:40 +0200

    Server: Add authMiddleware.js to check if token exists and is valid.

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header('x-auth-token');
+
+  if (!token) res.status(401).json({ msg: 'No token present' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res
+      .status(400)
+      .json({ msg: 'Token is not valid' })
+      .json(e);
+  }
+};
+
+module.exports = authMiddleware;
