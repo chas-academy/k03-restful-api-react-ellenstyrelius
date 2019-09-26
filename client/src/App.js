1776f7a15b42075d49c6e55aab1482603e3def27
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-09-26 16:16:48 +0200

    Client: Add components folder where all the components that are not utils/building blocks will go. Might re-structure this later. Create NavBar component and SearchField component, with styling and all. Render SearchField in NavBar and render NavBar in App. Change some universal styling in index.css.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 
-import size from './utils/styling/size';
+import NavBar from './components/NavBar';
+
 import color from './utils/styling/color';
-import fontSize from './utils/styling/fontSize';
-import { ReactComponent as Splash } from './utils/splash.svg';
-
-const Container = styled.div`
-  padding: calc(7 * ${size.small} + ${size.medium});
-`;
+import { ReactComponent as Splash } from './utils/img/splash.svg';
 
 function App() {
   return (
     <>
-      <Container>
-        <p style={{ fontSize: fontSize.large }}>
-          Coming soon: Tingling Tints webshop (°-°)
-        </p>
-      </Container>
+      <NavBar />
       <Splash height={160} fill={color.oldPeach} />
     </>
   );
---
+++ client/src/components/SearchField.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as SearchIcon } from '../utils/img/search.svg';
+import color from '../utils/styling/color';
+import size from '../utils/styling/size';
+import fontSize from '../utils/styling/fontSize';
+
+const { offBlack, gray3 } = color;
+const { min, small, large } = size;
+
+const Container = styled.div`
+  background: white;
+  margin-right: ${large};
+  border: 1px solid ${gray3};
+  display: flex;
+  align-items: center;
+`;
+
+const Input = styled.input`
+  width: calc(6 * ${large});
+  margin: ${min} ${small};
+  color: ${offBlack};
+  font-size: ${fontSize.fontSmall};
+  border: none;
+  :focus {
+    outline: none;
+  }
+`;
+
+function SearchField() {
+  return (
+    <Container>
+      <Input placeholder="search for tints" />
+      <SearchIcon />
+    </Container>
+  );
+}
+
+export default SearchField;
