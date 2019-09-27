af947b8ab95465b954d264c6ccd541ce08115fc2
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-09-27 14:11:21 +0200

    Client: Move styling for root div from index.css to App.js. Leaving only the wildcard selector with margin, padding and box-sizing properties in index.css.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
+import color from './utils/styling/color';
 import NavBar from './components/NavBar';
 import Loader from './components/Loader';
 import apiUrl from './utils/apiUrl';
+
+const AppContainer = styled.div`
+  background: ${color.offWhite};
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  font-family: 'Roboto', 'Helvetica Neue', Helvetica, sans-serif;
+  font-weight: 200;
+`;
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -22,12 +35,10 @@ function App() {
   }, []);
 
   return (
-    <>
+    <AppContainer>
       <NavBar />
       {isLoading && <Loader />}
-      <p>coming soon: Tingling Tints webshop</p>
-      <div></div>
-    </>
+    </AppContainer>
   );
 }
 
