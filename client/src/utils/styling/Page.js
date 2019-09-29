cbeb04cf05cb0fa7c8d8a32da3b52973a426d05f
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-09-29 13:26:34 +0200

    Client: Try to fix minor styling issues with jumping navbar due to scrollbar and margin for Page component.

--- client/src/utils/styling/Page.js
+++ client/src/utils/styling/Page.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import heightOfNav from '../heightOfNav';
 
+const height = heightOfNav + 100;
+
 const Page = styled.section`
   width: 96%;
-  margin-top: ${heightOfNav}px;
+  margin-top: ${height}px;
   display: flex;
   flex-direction: column;
   align-items: center;
